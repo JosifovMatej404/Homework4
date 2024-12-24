8a60bf873dc7d39d7f4f7de17bde03db1997afe6
Matej Josifov <62450091+JosifovMatej404@users.noreply.github.com>
2024-12-25 00:32:50 +0100

    integrated selenium worker

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
 from Scraper import SeleniumWorker
 
-worker = SeleniumWorker()
-worker.fetch_data_with_dates_and_key("01.01.2023", "31.12.2023", "alkb")
-print(obj.text + "\n" for obj in worker.web_objects)
+data = []
+key_worker = SeleniumWorker()
+keys = key_worker.fetch_company_keys()
+while keys is None: continue
+with ThreadPoolExecutor(max_workers=10) as executor:
+    # Submit tasks to the executor and collect futures
+    worker = SeleniumWorker()
+    futures = {executor.submit(worker.fetch_data_for_10_years_with_key(key)): key for key in keys}
+    for future in as_completed(futures):
+        try:
+            result = future.result()  # Get the return value from process_item
+            data.append(result)
+        except Exception as e:
+            print(f"Task raised an exception: {e}")
+
+for i in range(len(keys)):
+    print(keys[i] + "\n")
+    print(data[i])
+    print("\n")
+
